96543d6279a8e7cc047d1c20a3c554343fd4e8a1
Ramy Mohammed <ramyamohammed@outlook.com>
2026-05-10 19:43:44 -0400

    i hate node.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  envDir: resolve(__dirname, '../..'),
   plugins: [react()],
 })
